fdd653527059225cc4d46627dacdcac63819e5fa
khushikunte <93979917+khushikunte@users.noreply.github.com>
2025-01-01 08:12:51 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,15 +2,17 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 import requests
+import re
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie :cup_with_straw:")
 st.write(
     """  Choose the fruits you want in your custom Smoothie !
     """
 )
-name_on_order= st.text_input('Name on Smoothie:')
-st.write('The name on your Smoothie will be: ',name_on_order)
+name_on_order = st.text_input('Name on Smoothie:')
+cleaned_name = re.sub(r'["\']', '', name_on_order.strip())  # Remove quotes and spaces
 
+st.write(f'The cleaned name on your Smoothie will be: {cleaned_name}')
 cnx =st.connection("snowflake")
 session= cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit_name'),col('search_on'))
